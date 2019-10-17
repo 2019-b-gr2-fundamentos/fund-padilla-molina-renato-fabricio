3dd759c2f2f5dd52445c6de883750a90917f1510
Reno956 <renato.padilla@epn.edu.ec>
2019-10-17 17:53:40 -0500

    Variables primitivas

---
+++ 01-Lenguajes/01-variables.js
@@ -0,0 +1,29 @@
+//Comentario
+/*
+Nombre: Renato Padilla
+Fecha de actualizacion: 17-10-2019
+Descripccion: Introduccion a variables
+*/
+/* abro bloque de comentario
+
+cierro bloque de comentario */
+// Primitivas (Tipos de variables)
+/*
+int         -> Entero       Ej: 5 4 7 -9
+char        -> Caracter     Ej: a d g t
+float       -> Decimal      Ej: 9.34 -7.24
+double      -> Decimal ++   Ej: 1212...
+string      -> Arreglo char Ej: "absdsdas"
+null        -> Vacio        Ej: null   
+boolean     -> True False   Ej: 1 0
+*/
+// var edad = 30; // nunca usar
+//let edad = 30; // Mutable
+const edad = 30; // Inmutable
+const IVA = 12;
+//IVA = 14;
+let sueldo = 0;
+// var edad: Integer = 30; -> kotlin Mutable
+// val edad: Integer = 30; -> kotlin Inutable
+//let IVA = 12;
+//const IVA = 12;

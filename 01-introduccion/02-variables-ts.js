0ff162605e8e2d7ad9f6861b5dfb1084231314f7
Reno956 <renato.padilla@epn.edu.ec>
2019-10-21 15:53:32 -0500

    Variables Primitvas 02

---
+++ 01-introduccion/02-variables-ts.js
@@ -0,0 +1,9 @@
+// Variables Primitivas
+const numero = 10;
+const decimal = 10.12;
+const repuestaTipoNumero = typeof numero;
+console.log(repuestaTipoNumero);
+console.log(typeof decimal);
+// Destor de paquetes de nodejs
+// Comunicarse con el servidor
+// npm - yarn
--- 01-introduccion/02-variables-ts.ts
+++ 01-introduccion/02-variables-ts.ts
@@ -1,8 +1,24 @@
-var edad = 30;
-let edad = 30; 
-const edad = 30; 
-const IVA = 12;
-IVA = 14;
-let sueldo = 0;
-let IVA = 12;
-const IVA = 12;
+// Variables Primitivas
+const numero=10;
+const decimal=10.12;
+const repuestaTipoNumero=typeof numero;
+console.log(repuestaTipoNumero);
+console.log(typeof decimal);
+const letra="a";
+const palabra="palabra";
+console.log(typeof letra);
+console.log(typeof palabra);
+const hijos=null;
+console.log(typeof hijos);
+const casado=true;
+console.log(typeof casado);
+const fecha=new Date();
+console.log(typeof fecha);
+
+
+
+
+// Destor de paquetes de nodejs
+// Comunicarse con el servidor
+// Descargar el "paquete"
+// npm - yarn

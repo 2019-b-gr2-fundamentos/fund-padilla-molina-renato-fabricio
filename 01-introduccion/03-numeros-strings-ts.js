4964b38412fc90c38cbd36f9c48459483fe475ba
Reno956 <renato.padilla@epn.edu.ec>
2019-10-26 15:08:00 -0500

    24-10-2019

--- 01-introduccion/03-numeros-strings-ts.js
+++ 01-introduccion/03-numeros-strings-ts.js
@@ -1,6 +1,6 @@
-const numeroUno = 1; //camelCase
-const numeroDos = 2;
-let resultado = 0;
+var numeroUno = 1; //camelCase
+var numeroDos = 2;
+var resultado = 0;
 // SUMA
 resultado = 1 + 2;
 // RESTAR
@@ -14,6 +14,39 @@ resultado = 1 ^ 2;
 // MODULO
 resultado = 4 % 2; // ->Residuo
 console.log("pera" + 2);
+// STRINGS
+var mensaje = "\"Hola mundo\"";
+var saludo = '\'Adio\s mundo\'';
+console.log(mensaje + "1\t\t\t 3\n" + saludo);
+// template strings
+var edad = 30;
+var saludoEdad = "Mi edad es: \n" + edad;
+var saludoCompleto = mensaje + "\n" + saludo;
+console.log(saludoEdad);
+console.log(saludoCompleto);
+// Contamos los elementos
+var nombreLongitud = " Renato Fabricio ";
+console.log(nombreLongitud.length);
+// Eliminar espacios al rpincipio y al final
+console.log(nombreLongitud.trim());
+// Cambiar a mayusculas o minusculas
+console.log(nombreLongitud.toUpperCase());
+console.log(nombreLongitud.toLowerCase());
+//console.log(nombreLongitud.replace("e","o"));
+//console.log(nombreLongitud.replace("a","o"));
+//console.log(nombreLongitud.replace("\n",""));
+console.log(nombreLongitud);
+console.log(nombreLongitud.replace("x", "o"));
+console.log(nombreLongitud);
+console.log(nombreLongitud.replace("a", "o"));
+console.log(nombreLongitud);
+console.log(nombreLongitud.search("t"));
+console.log(nombreLongitud.search("Renato"));
+console.log(nombreLongitud.slice(0, 7)); // Impime solo el numero que se quiere
+console.log(nombreLongitud.indexOf("t"));
+console.log(nombreLongitud.includes("ren", 0)); // Dice si incluye un caracter o no
+//console.log(nombreLongitud.endsWith("x"));
+//console.log(nombreLongitud.startsWith("Reno",8));
 // NUMEROUNO -> TODO MAYUSCULA
 // NUMERO_UNO -> TODO MAYUSCULAS GUION BAJO
 // NuemroUno -> Primera letra mayuscula
--- 01-introduccion/03-numeros-strings-ts.ts
+++ 01-introduccion/03-numeros-strings-ts.ts
@@ -14,6 +14,39 @@ resultado=1^2;
 // MODULO
 resultado=4%2;// ->Residuo
 console.log("pera"+2);
+// STRINGS
+const mensaje="\"Hola mundo\"";
+const saludo='\'Adio\s mundo\'';
+console.log(mensaje+"1\t\t\t 3\n"+saludo);
+// template strings
+const edad=30;
+const saludoEdad=`Mi edad es: \n${edad}`;
+const saludoCompleto=`${mensaje}\n${saludo}`;
+console.log(saludoEdad);
+console.log(saludoCompleto);
+// Contamos los elementos
+const nombreLongitud=" Renato Fabricio ";
+console.log(nombreLongitud.length);
+// Eliminar espacios al rpincipio y al final
+console.log(nombreLongitud.trim());
+// Cambiar a mayusculas o minusculas
+console.log(nombreLongitud.toUpperCase());
+console.log(nombreLongitud.toLowerCase());
+//console.log(nombreLongitud.replace("e","o"));
+//console.log(nombreLongitud.replace("a","o"));
+//console.log(nombreLongitud.replace("\n",""));
+console.log(nombreLongitud);
+console.log(nombreLongitud.replace("x","o"));
+console.log(nombreLongitud);
+console.log(nombreLongitud.replace("a","o"));
+console.log(nombreLongitud);
+console.log(nombreLongitud.search("t"));
+console.log(nombreLongitud.search("Renato"));
+console.log(nombreLongitud.slice(0,7)); // Impime solo el numero que se quiere
+console.log(nombreLongitud.indexOf("t")); 
+console.log(nombreLongitud.includes("ren",0)); // Dice si incluye un caracter o no
+//console.log(nombreLongitud.endsWith("x"));
+//console.log(nombreLongitud.startsWith("Reno",8));
 
 
 

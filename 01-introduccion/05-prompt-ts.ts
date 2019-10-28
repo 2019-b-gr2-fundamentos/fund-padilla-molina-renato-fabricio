7b327054ad8afd1e5500a678c3789039089dcc35
Reno956 <renato.padilla@epn.edu.ec>
2019-10-28 16:02:14 -0500

    Condicionales

---
+++ 01-introduccion/05-prompt-ts.ts
@@ -0,0 +1,5 @@
+const numeroUnoString=prompt("Ingresa numero 1");
+const numeroDosString=prompt("Ingresa numero 2");
+const numeroUno=Number(numeroUnoString);
+const numeroDos=Number(numeroDosString);
+console.log(numeroUno+numeroDos);

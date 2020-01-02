34f858982453426ac166e5a8d3e402f8d5749128
Reno956 <renato.padilla@epn.edu.ec>
2020-01-02 17:32:13 -0500

    Validaciones

--- 03-funciones/04-matrices.ts
+++ 03-funciones/04-matrices.ts
@@ -1,14 +1,62 @@
-const arregloMatriz = [
-    [1,2],
-    [3,4,5],
-    [6,7,8,6],
-    [9],
-    [],
-];
+function intercambiarDiag(matrizUno: number[][]): void{
+    const esValido= verificarTodosLosElementosDeUnArregloSonArreglo(matrizUno);
+    if(esValido){
+        let diagonalUno=[];
+        let diagonalDos=[];
+        let contadorI=matrizUno[0].length;
+        for(let contadorJ=0;contadorJ<matrizUno[0].length;contadorJ++){
+            diagonalUno[contadorJ]=matrizUno[contadorJ][contadorJ];       
+        }
+        for(let contadorJ=0;contadorJ<matrizUno[0].length;contadorJ++){
+            contadorI--; 
+            diagonalDos[contadorI]=matrizUno[contadorJ][contadorI];  
+        }
+        contadorI=matrizUno[0].length;
+        for(let contadorJ=0;contadorJ<matrizUno[0].length;contadorJ++){
+            matrizUno[contadorJ][contadorJ]=diagonalDos[contadorJ];       
+        }
+        for(let contadorJ=0;contadorJ<matrizUno[0].length;contadorJ++){
+            contadorI--; 
+            matrizUno[contadorJ][contadorI]=diagonalUno[contadorI];  
+        }
+        for(let contadorJ=0;contadorJ<matrizUno.length;contadorJ++){
+            console.log(matrizUno[contadorJ])      
+        }
+    }
+}
 
+function sonIguales(valor: boolean,matrizUno: number[][],matrizDos: number[][]): void{
+    let validar=0;
+    if(valor==true){
+        for(let contadorI=0;contadorI<matrizUno.length;contadorI++){
+            for(let contadorJ=0;contadorJ<matrizUno[0].length;contadorJ++){
+                if(matrizUno[contadorI][contadorJ]==matrizDos[contadorI][contadorJ]){
+                    validar++;
+                }else{
+                    validar=0;
+                }
+            }
+        }
+        if(validar!=0){
+            console.log("Las matrices son iguales");
+        }else{
+            console.log("Las no matrices son iguales");
+        }
+    }else {
+        console.log("Las dimensiones no son iguales");
+    }
+    
+} 
+function comparaMatriz(matrizUno: number[][],matrizDos: number[][]): boolean {
+    const esValido=tienenMtricesIgualDim(matrizUno,matrizDos);
+    if(esValido){
+        return true;
+    }else {
+        return false;
+    }
+}
 
-
-function compararMatriz(matrizUno: number[][],matrizDos: number[][]): boolean {
+function tienenMtricesIgualDim(matrizUno: number[][],matrizDos: number[][]): boolean {
     const matrizUnoPrimeraDimension = obtenerPrimeraDimension(
         matrizUno
         );
@@ -21,12 +69,17 @@ function compararMatriz(matrizUno: number[][],matrizDos: number[][]): boolean {
     const matrizDosSegundaDimension = obtenerSegundaDimension(
         matrizDos
         );
-    console.log(matrizUnoPrimeraDimension);
-    console.log(matrizUnoSegundaDimension);
-    console.log(matrizDosPrimeraDimension);
-    console.log(matrizDosSegundaDimension);
-    
- return true;
+    const noHayFalsos=matrizUnoPrimeraDimension !=false && matrizUnoSegundaDimension !=false && matrizDosPrimeraDimension !=false && matrizDosSegundaDimension !=false;
+    const tienenIgualDim=matrizUnoPrimeraDimension == matrizDosPrimeraDimension && matrizUnoSegundaDimension == matrizDosSegundaDimension;
+    if(noHayFalsos){
+            if(tienenIgualDim){
+                return true;
+            } else {
+                return false;
+            }
+        } else{
+            return false;
+        }
 }
 
 function obtenerPrimeraDimension(matrizUno: number[][]): number | boolean{
@@ -81,16 +134,19 @@ function verificarTodosLosElementosDeUnArregloSonArreglo(arreglo: any[]):boolean
     return true;
 }
 
-
 function main(){
     const x = [
-        [1,2],
-        [3]
+        [1,2,9],
+        [3,2,7],
+        [4,5,6]
     ];
     const y = [
-        [1,2],
-        [3,4],
+        [1,2,9],
+        [3,2,7],
+        [4,5,6]
     ];
-    compararMatriz(x, y);
+    const res=comparaMatriz(x, y);
+    sonIguales(res,x,y);
+    intercambiarDiag(x);
 }
 main();

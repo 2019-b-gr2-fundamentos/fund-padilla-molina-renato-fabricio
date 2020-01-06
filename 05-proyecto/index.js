85f5c53c9bb9c726235a6f92c7d356c3bfb55dd2
Reno956 <renato.padilla@epn.edu.ec>
2020-01-06 16:04:15 -0500

    Librerias

---
+++ 05-proyecto/index.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+//const sumaLib=require('reno-lib-1/lib/suma');
+var sumaLib = require("reno-lib-1/lib/suma");
+console.log("La respuesta es: " + sumaLib(4, 7));
